af20df385ad7d4af957a4674b0e75e6e55354d53
Nicolas Zhang <biazhang@microsoft.com>
2017-08-03 11:15:59 +0800

    check admin access before running

--- script-update-gateway.ps1
+++ script-update-gateway.ps1
@@ -171,6 +171,13 @@ function Is-64BitSystem
      return $osBit -eq '64'
 }
 
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+    [Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
+    Break
+}
+
 $currentVersion = Get-CurrentGatewayVersion
 if ($currentVersion -eq $null)
 {
