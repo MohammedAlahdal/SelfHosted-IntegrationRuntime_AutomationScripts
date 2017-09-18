3cac153bba7d030536229d3ff1dad358ececc44a
Nicolas Zhang <biazhang@microsoft.com>
2017-09-18 14:57:18 +0800

    update script to install gateway locally

--- InstallGatewayOnLocalMachine.ps1
+++ InstallGatewayOnLocalMachine.ps1
@@ -1,29 +1,28 @@
 #### Here is the usage doc:
-#### PS D:\GitHub> .\InstallGatewayOnLocalMachine.ps1 E:\shared\bugbash\IntegrationRuntime.msi
+#### PS D:\GitHub> .\InstallGatewayOnLocalMachine.ps1 E:\shared\bugbash\IntegrationRuntime.msi <key>
 ####
 
-param([string]$path)
+param([string]$path, [string]$authKey)
 function Install-Gateway([string] $gwPath)
 {
-    if ([string]::IsNullOrEmpty($gwPath))
-    {
-        throw "Gateway path is not specified"
-    }
-
-    if (!(Test-Path -Path $gwPath))
-    {
-        throw "Invalid gateway path: $gwPath"
-    }
-
     # uninstall any existing gateway
     UnInstall-Gateway
 
     Write-Host "Start Gateway installation"
     
-    Start-Process "msiexec.exe" "/i $path /quiet /passive"
+    Start-Process "msiexec.exe" "/i $path /quiet /passive" -Wait
     Start-Sleep -Seconds 30	
 
-    Write-Host "Installation of gateway is successful"
+    Write-Host "Succeed to install gateway"
+}
+
+function Register-Gateway([string] $key)
+{
+    Write-Host "Start to register gateway with key: $key"
+    $cmd = Get-CmdFilePath
+    Start-Process $cmd "-k $key" -Wait
+    Write-Host "Succeed to register gateway"
+
 }
 
 function Check-WhetherGatewayInstalled([string]$name)
@@ -63,8 +62,36 @@ function UnInstall-Gateway()
         return
     }
 
-    Write-Host "Microsoft Data Management Gateway Preview has been uninstalled from this machine."
-    Start-Sleep -Seconds 30
+    Write-Host "Microsoft Data Management Gateway has been uninstalled from this machine."
+}
+
+function Get-CmdFilePath()
+{
+    $filePath = Get-ItemPropertyValue "hklm:\Software\Microsoft\DataTransfer\DataManagementGateway\ConfigurationManager" "DiacmdPath"
+    if ([string]::IsNullOrEmpty($filePath))
+    {
+        throw "Get-InstalledFilePath: Cannot find installed File Path"
+    }
+
+    return $filePath
+}
+
+function Validate-Input([string]$path, [string]$key)
+{
+    if ([string]::IsNullOrEmpty($path))
+    {
+        throw "Gateway path is not specified"
+    }
+
+    if (!(Test-Path -Path $path))
+    {
+        throw "Invalid gateway path: $path"
+    }
+
+    if ([string]::IsNullOrEmpty($key))
+    {
+        throw "Gateway Auth key is empty"
+    }
 }
 
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
@@ -74,4 +101,7 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Break
 }
 
+Validate-Input $path $authKey
+
 Install-Gateway $path
+Register-Gateway $authKey
